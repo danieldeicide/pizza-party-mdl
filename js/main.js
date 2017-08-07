8961c05ad8743de116d0aade587b8865763515a7
Daniel Turus <danideicide@gmail.com>
2017-08-07 19:07:13 +0300

    dsadsad
    ssadsa

--- js/main.js
+++ js/main.js
@@ -3,8 +3,9 @@ $( document ).ready(function(){
 })
 $(".button-collapse").sideNav();
 $('.button-collapse').sideNav({
-    menuWidth: 300, // Default is 240
+    menuWidth: 240, // Default is 240
     edge: 'left', // Choose the horizontal origin
+    draggable: false,
     closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
   }
 );
