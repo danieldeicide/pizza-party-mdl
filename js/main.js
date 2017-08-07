ae521140e355c98fc117b4a40269abbca224a588
Daniel Turus <danideicide@gmail.com>
2017-08-07 19:15:53 +0300

    dsadsa
    dsadsa

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 $( document ).ready(function(){
   $('.parallax').parallax();
 })
-$(".button-collapse").sideNav();
+getPanelWidth = function() {
+    var screenWidth = $(window).width();
+    panelSize = screenWidth > '991' ? Math.floor(screenWidth * 0.5) : Math.floor(screenWidth * 0.7);
+    console.log(panelSize);
+    return panelSize;
+};
 $('.button-collapse').sideNav({
-    menuWidth: 240, // Default is 240
+    menuWidth: getPanelWidth(),
     edge: 'left', // Choose the horizontal origin
     draggable: false,
-    closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    closeOnClick: true
   }
 );
   $('.carousel.carousel-slider').carousel({fullWidth: true });
